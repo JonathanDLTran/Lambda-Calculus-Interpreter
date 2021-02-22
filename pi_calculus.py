fdc49a21363b29e5bd8f5adfa3f4fe12cd973c89
Jonathan Tran <52905762+JonathanDLTran@users.noreply.github.com>
2021-02-21 22:51:33 -0500

    finished simple interpreter?

--- pi_calculus.py
+++ pi_calculus.py
@@ -8,7 +8,8 @@ including the Operational Semantics
 
 # ------------ IMPORTS ------------
 import threading
-import random
+from random import shuffle
+from copy import deepcopy
 
 
 # ------------ AST CLASSES ------------
@@ -65,7 +66,7 @@ class Receive(Process):
         return self.channel
 
     def __str__(self):
-        return f"{self.channel}({self.var}).{self.proc}"
+        return f"@{self.channel}({self.var}).{self.proc}"
 
 
 class Send(Process):
@@ -87,7 +88,7 @@ class Send(Process):
         return self.channel
 
     def __str__(self):
-        return f"_{self.channel}_<{self.message}>.{self.proc}"
+        return f"->{self.channel}<{self.message}>.{self.proc}"
 
 
 class Or(Process):
@@ -103,6 +104,12 @@ class Or(Process):
 
     def get_right(self):
         return self.right
+
+    def set_left(self, left):
+        self.left = left
+
+    def set_right(self, right):
+        self.right = right
 
     def __str__(self):
         return f"{self.left} | {self.right}"
@@ -138,62 +145,137 @@ def gen_var():
     return new_var
 
 
-def subst(p, c, x):
-    pass
+def fv(proc):
+    assert isinstance(proc) == Process
+    if type(proc) == Zero:
+        return []
+    elif type(proc) == Send:
+        return [proc.get_channel(), proc.get_message()] + fv(proc.get_proc())
+    elif type(proc) == Receive:
+        return [proc.get_channel()] + list(filter(lambda v: v != proc.get_var(), fv(proc.get_proc())))
+    elif type(proc) == Or:
+        return fv(proc.get_left()) + fv(proc.get_right())
+    elif type(proc) == New:
+        return list(filter(lambda v: v != proc.get_var(), fv(proc.get_proc())))
+    elif type(proc) == Respawn:
+        return fv(proc.get_proc())
+    else:
+        raise RuntimeError(f"No Matching Case in FV: {proc}")
+
+
+def subst(proc, c, x, bound):
+    """
+    subst c for x in proc
+    """
+    assert isinstance(proc, Process)
+
+    if type(proc) == Zero:
+        return proc
+    elif type(proc) == Send:
+        channel = proc.get_channel()
+        msg = proc.get_message()
+        sub_proc = proc.get_proc()
+        new_channel = c if x == channel and channel not in bound else channel
+        new_msg = c if x == msg and msg not in bound else msg
+        new_sub_proc = subst(sub_proc, c, x, deepcopy(bound))
+        new_proc = Send(new_channel, new_msg, new_sub_proc)
+        return new_proc
+    elif type(proc) == Receive:
+        channel = proc.get_channel()
+        var = proc.get_var()
+        sub_proc = proc.get_proc()
+        new_channel = c if x == channel and channel not in bound else channel
+        bound.add(var)
+        new_sub_proc = subst(sub_proc, c, x, deepcopy(bound))
+        new_proc = Send(new_channel, var, new_sub_proc)
+        return new_proc
+    elif type(proc) == Or:
+        new_left = subst(proc.get_left(), c, x, deepcopy(bound))
+        new_right = subst(proc.get_right(), c, x, deepcopy(bound))
+        new_proc = Or(new_left, new_right)
+        return new_proc
+    elif type(proc) == New:
+        var = proc.get_var()
+        sub_proc = proc.get_proc()
+        bound.add(var)
+        new_sub_proc = subst(sub_proc, c, x, deepcopy(bound))
+        new_proc = New(var, new_sub_proc)
+        return new_proc
+    elif type(proc) == Respawn:
+        new_body = subst(proc.get_proc(), c, x, deepcopy(bound))
+        new_proc = Respawn(new_body)
+        return new_proc
+    else:
+        raise RuntimeError(f"No Matching Case in Subst: {proc}")
 
 
 def interpret(state):
+    print(state)
     assert type(state) == tuple
     assert len(state) == 2
     (C, R) = state
-    has_zero = False
-    has_or = False
-    has_respawn = False
-    has_new = False
-    for proc in R:
+    shuffle(R)
+    has_zero = -1
+    has_or = -1
+    has_respawn = -1
+    has_new = -1
+    has_send = -1
+    has_receive = -1
+    for i in range(len(R)):
+        proc = R[i]
         if type(proc) == Zero:
-            has_zero = True
+            has_zero = i
         elif type(proc) == Or:
-            has_or = True
+            has_or = i
         elif type(proc) == New:
-            has_new = True
+            has_new = i
         elif type(proc) == Respawn:
-            has_respawn = True
-    if has_zero:
-        new_R = list(filter(lambda proc: type(proc) != Zero, R))
+            has_respawn = i
+        elif type(proc) == Send:
+            has_send = i
+        elif type(proc) == Receive:
+            has_receive = i
+    if has_zero >= 0:
+        new_R = R[:has_zero] + R[has_zero + 1:]
         new_state = (C, new_R)
         return interpret(new_state)
-    elif has_or:
-        new_R = []
-        for proc in R:
-            if type(proc) == Or:
-                new_R.append(proc.get_left())
-                new_R.append(proc.get_right())
-            else:
-                new_R.append(proc)
+    elif has_or >= 0:
+        proc = R[has_or]
+        new_R = R[:has_or] + [proc.get_left(), proc.get_right()] + \
+            R[has_or + 1:]
         new_state = (C, new_R)
         return interpret(new_state)
-    elif has_new:
-        for i in range(len(R)):
-            proc = R[i]
-            if type(proc) == New:
-                p = proc.get_proc()
-                x = proc.get_var()
-                c = gen_var()
-                new_C = C.add(c)
-                new_R = R[:i] + R[i + 1:]
-                new_R.append(subst(p, c, x))
-                new_state = (new_C, new_R)
-                return interpret(new_state)
+    elif has_new >= 0:
+        proc = R[has_new]
+        p = proc.get_proc()
+        x = proc.get_var()
+        c = gen_var()
+        C.add(c)
+        new_R = R[:has_new] + R[has_new + 1:]
+        new_R.append(subst(p, c, x, set()))
+        new_state = (C, new_R)
+        return interpret(new_state)
+    elif has_send >= 0 and has_receive >= 0:
+        q = R[has_send]
+        p = R[has_receive]
+        q_proc = q.get_proc()
+        q_msg = q.get_message()
+        q_chan = q.get_channel()
+        p_proc = p.get_proc()
+        p_var = p.get_var()
+        p_chan = p.get_channel()
+        if p_chan != q_chan:
+            return interpret(state)
+        new_R = R[:min(has_receive, has_send)] + \
+            R[min(has_receive, has_send): max(has_receive, has_send)] + \
+            R[max(has_receive, has_send)] + \
+            [q_proc] + \
+            [subst(p_proc, q_msg, p_var, set())]
+        new_state = (C, new_R)
+        return interpret(new_state)
     # make last because respawn could just go forever
-    elif has_respawn:
-        new_R = []
-        for proc in R:
-            if type(proc) == Respawn:
-                new_R.append(proc)
-                new_R.append(proc.get_proc())
-            else:
-                new_R.append(proc)
+    elif has_respawn >= 0:
+        new_R = R[:has_respawn] + [proc, proc.get_proc()] + R[has_respawn + 1:]
         new_state = (C, new_R)
         return interpret(new_state)
     return 1
@@ -210,7 +292,8 @@ def interpret_main(process):
 # ------------ MAIN RUNNEr ------------
 
 def main():
-    process = Or(Zero(), Zero())
+    process = Or(Or(Send("x", "y", Zero()), New(
+        "x", Receive("x", "x", Zero()))), Zero())
     return interpret_main(process)
 
 
