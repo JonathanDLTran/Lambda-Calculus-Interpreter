6c796fa3e71df9668572474abad934b0441486a5
Jonathan Tran <52905762+JonathanDLTran@users.noreply.github.com>
2021-06-14 23:08:54 -0700

    Add files via upload

---
+++ diff_lex.py
@@ -0,0 +1,30 @@
+ADD = "+"
+SUB = "-"
+MUL = "*"
+DIV = "\\"
+POW = "^"
+
+DOT = "."
+
+LN = "ln"
+SIN = "sin"
+COS = "cos"
+
+PI = "pi"
+EXP = "e"
+
+
+def lex(string):
+    assert type(string) == str
+
+
+def main():
+    pass
+
+
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    test()
