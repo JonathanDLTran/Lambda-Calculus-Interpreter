532f32d8b6289294042f6b7821a44f3ef4c5a83e
Jonathan Tran <52905762+JonathanDLTran@users.noreply.github.com>
2021-11-10 23:52:29 -0500

    Basic conversion for base types and binary operators

--- cps.py
+++ cps.py
@@ -1,8 +1,10 @@
 """
 cps.py translates scheme code in continuation passing style.
 """
-LAMBDA = "lambda"
-APPLY = "apply"
+from constants import *
+from scheme_classes import *
+from eval import eval_expr
+
 K = "k"
 K_COUNTER = 0
 
@@ -16,22 +18,19 @@ def gen_k():
 def to_cps(expr):
     if type(expr) == int:
         var = gen_k()
-        return [LAMBDA, var, [APPLY, var, expr]]
+        return [LAMBDA, [var], [var, expr]]
     elif type(expr) == bool:
         var = gen_k()
-        return [LAMBDA, var, [APPLY, var, expr]]
+        return [LAMBDA, [var], [var, expr]]
     elif expr == NIL:
         var = gen_k()
-        return [LAMBDA, var, [APPLY, var, expr]]
+        return [LAMBDA, [var], [var, expr]]
     elif type(expr) == str:
-        if expr in ctx:
-            return ctx[expr]
-        # this is a symbol under quasi
-        elif in_quasi:
-            return expr
+        var = gen_k()
+        return [LAMBDA, [var], [var, expr]]
     elif type(expr) == String:
         var = gen_k()
-        return [LAMBDA, var, [APPLY, var, expr]]
+        return [LAMBDA, [var], [var, expr]]
     elif type(expr) == Values:
         raise RuntimeError(
             f"Cannot evaluate Values Internal Class any further; {expr}.")
@@ -42,34 +41,32 @@ def to_cps(expr):
 
     first = expr[0]
     # Named Arithmetic Operators
-    if first == ADD:
-        return eval_add(expr, ctx, in_quasi)
-    elif first == SUB:
-        return eval_sub(expr, ctx, in_quasi)
-    elif first == MUL:
-        return eval_mul(expr, ctx, in_quasi)
-    elif first == DIV:
-        return eval_div(expr, ctx, in_quasi)
-    elif first == EXP:
-        return eval_exp(expr, ctx, in_quasi)
-    elif first == CONCAT:
-        return eval_concat(expr, ctx, in_quasi)
-    # Named Special Forms
-    elif first == QUOTE:
-        return eval_quote(expr, ctx, in_quasi)
-    elif first == PRINTLN:
-        return eval_println(expr, ctx, in_quasi)
-    elif first == SET:
-        return eval_set(expr, ctx, in_quasi)
-    elif first == EQ:
-        return eval_eq(expr, ctx, in_quasi)
+    if first in [ADD, SUB, MUL, DIV, EXP, CONCAT]:
+        k = gen_k()
+        first_arg = expr[1]
+        var1 = gen_k()
+        second_arg = expr[2]
+        var2 = gen_k()
+        return [LAMBDA, [k], [to_cps(first_arg), [LAMBDA, [var1], [to_cps(second_arg), [LAMBDA, [var2], [k, [first, var1, var2]]]]]]]
+    # # Named Special Forms
+    # elif first == QUOTE:
+    #     return eval_quote(expr, ctx, in_quasi)
+    # elif first == PRINTLN:
+    #     return eval_println(expr, ctx, in_quasi)
+    # elif first == SET:
+    #     return eval_set(expr, ctx, in_quasi)
+    # elif first == EQ:
+    #     return eval_eq(expr, ctx, in_quasi)
 
 
 def main():
-    program = []
+    program = [SUB, [ADD, 1, 2], 2]
     cps = to_cps(program)
-    starting = [LAMBDA, K, K]
-    return [APPLY, cps, starting]
+    starting = [LAMBDA, [K], K]
+    result = [cps, starting]
+    print(result)
+    evaled = eval_expr(result, {}, False)
+    print(evaled)
 
 
 if __name__ == "__main__":
