387a454826aa31a30a67304a906e69e2d7a045fe
Jonathan Tran <52905762+JonathanDLTran@users.noreply.github.com>
2022-04-29 02:00:21 -0400

    Add files via upload

--- scope.ml
+++ scope.ml
@@ -1,10 +1,16 @@
-type lambda = 
+type ctx = (string * lambda) list
+
+and lambda = 
   | Int of int 
   | Plus of lambda * lambda 
   | Let of string * lambda * lambda 
   | Var of string 
   | Abs of string * lambda 
   | App of lambda * lambda
+  | DynCtx of ctx
+  | Lookup of string * string 
+  | Update of string * lambda * string 
+
 
 type value = 
   | VInt of int
@@ -18,6 +24,10 @@ let rec expr_to_str expr =
   | Abs(x, e) -> "fun " ^ x ^ " -> " ^ expr_to_str e
   | App(e1, e2) -> "(" ^ expr_to_str e1 ^ ") (" ^ expr_to_str e2 ^ ")"
   | Let(x, e1, e2) -> failwith "Lets need to already have been desugared"
+  | DynCtx _ -> "{DynCtx}"
+  | Lookup(ctx, x) -> "(lookup " ^ ctx ^ " " ^ x ^ ")"
+  | Update(ctx, v, x) -> "(update " ^ ctx ^ " " ^ expr_to_str v ^ " " ^ x ^ ")"
+
 
 let val_to_str value = 
   match value with 
@@ -32,18 +42,19 @@ let rec fv expr =
   | Plus(e1, e2) -> fv e1 @ fv e2 
   | Abs(x, e) ->  List.filter (fun elt -> elt <> x) (fv e)
   | Let(v, f, s) -> failwith "Lets need to already have been desugared"
+  | DynCtx _ -> failwith "No Dynamic Context Handling"
 
 let gen_new_var = 
   let count = ref 0 in 
-  fun () -> incr count; "x_ " ^ string_of_int !count
+  fun () -> incr count; "x_" ^ string_of_int !count
 
-(* Substitute e1 for x in e2 *)
+(* Substitute e1 for x in e2 = e2{e1/x} *)
 let rec subst x e1 e2 = 
   match e2 with 
   | Int _ -> e2 
   | Var y -> if y = x then e1 else e2 
-  | App(l, r) -> App(subst x e1 l, subst x e2 r)
-  | Plus(l, r) -> Plus(subst x e1 l, subst x e2 r)
+  | App(l, r) -> App(subst x e1 l, subst x e1 r)
+  | Plus(l, r) -> Plus(subst x e1 l, subst x e1 r)
   | Let(v, f, s) -> failwith "Lets need to already have been desugared"
   | Abs(y, e0) -> begin 
       if y = x then e2 
@@ -53,26 +64,28 @@ let rec subst x e1 e2 =
         let () = assert (y <> x); assert (z <> x); assert (not (List.mem z (fv e1))); assert (not (List.mem z (fv e0))) in
         Abs(z, subst x e1 (subst y (Var z) e0))
     end 
+  | DynCtx _ -> failwith "No Dynamic Context Handling"
 
 (* We can decompose let x = e1 in e2 as *lambda x. e1) e2 *)
-let rec eval expr ctx print = 
+let rec eval expr print = 
   let () = if print then expr |> expr_to_str |> print_endline else () in 
   match expr with 
   | Int n -> VInt n
   | Plus (l, r) -> begin 
-      match eval l ctx print, eval r ctx print with
+      match eval l print, eval r print with
       | VInt i1, VInt i2 -> VInt (i1 + i2)
       | _ -> failwith "Cannot add together non-ints"
     end
   | Let (v, e1, e2) -> failwith "Lets need to already have been desugared"
   | Abs(v, e) -> VAbs(v, e)
   | App(e1, e2) -> begin
-      match eval e1 ctx print with 
+      match eval e1 print with 
       | VAbs(x, e3) -> 
-        let beta = (subst x e2 e3) in eval beta ctx print
+        let beta = (subst x e2 e3) in eval beta print
       | _ -> failwith "First term in Application must be a function."
     end 
   | Var x -> failwith "Vars cannot be evaluated by themselves"
+  | DynCtx _ -> failwith "No Dynamic Context Handling"
 
 let rec remove_lets expr = 
   match expr with 
@@ -82,9 +95,65 @@ let rec remove_lets expr =
   | Abs(v, e) -> Abs(v, remove_lets e)
   | App(l, r) -> App(remove_lets l, remove_lets r) 
   | Let(x, f, s) -> (App (Abs(x, remove_lets s), remove_lets f))
+  | DynCtx _ -> failwith "No Dynamic Context Handling"
 
-let rec translate_to_dynamic expr = failwith "Unimplemented"
-let rec translate_to_static expr = failwith "Unimplemented"
+let rec translate_to_dynamic expr = 
+  match expr with 
+  | Int n -> fun ctx -> Int n
+  | Var x -> fun ctx -> begin  
+      match ctx with 
+      | DynCtx dynctx -> begin
+          match List.assoc_opt x dynctx with 
+          | Some value -> value 
+          | None -> failwith("Unbound Variable Name " ^ x ^ " in Lambda Term.")
+        end
+      | _ -> failwith "Not a Dynamic Context"
+    end 
+  | App(e1, e2) -> begin 
+      fun ctx -> 
+        App(App(translate_to_dynamic e1 ctx, translate_to_dynamic e2 ctx), ctx)
+    end
+  | Plus(e1, e2) ->  begin 
+      fun ctx -> 
+        App(Plus(translate_to_dynamic e1 ctx, translate_to_dynamic e2 ctx), ctx)
+    end
+  | Abs(x, e) -> begin 
+      let v = gen_new_var () in 
+      let t = gen_new_var () in 
+      fun ctx -> 
+        match ctx with 
+        | DynCtx dynctx -> begin
+            Abs(v, Abs(t, translate_to_dynamic e (DynCtx((x, Var(v))::List.remove_assoc x dynctx))))
+          end
+        | _ -> failwith "Not a Dynamic Context"
+    end
+  | DynCtx _ -> failwith "Cannot translate Dynamic Context"
+  | Let _ -> failwith "Lets need to already have been desugared"
+
+let rec translate_to_static expr = 
+  match expr with 
+  | Int n -> fun ctx -> Int n
+  | Var x -> 
+    fun ctx -> begin 
+        match List.assoc_opt x ctx with 
+        | Some value -> value 
+        | None -> failwith("Unbound Variable Name " ^ x ^ " in Lambda Term.")
+      end 
+  | App(e1, e2) -> begin 
+      fun ctx -> 
+        App(translate_to_static e1 ctx, translate_to_static e2 ctx)
+    end
+  | Abs(x, e) -> begin 
+      let v = gen_new_var () in 
+      fun ctx -> 
+        Abs(v, translate_to_static e ((x, Var(v))::(List.remove_assoc x ctx)))
+    end
+  | Plus(e1, e2) -> begin 
+      fun ctx -> 
+        Plus(translate_to_static e1 ctx, translate_to_static e2 ctx)
+    end
+  | Let _ -> failwith "Lets need to already have been desugared"
+  | DynCtx _ -> failwith "No Dynamic Context Handling"
 
 let main = 
   (* let prog = 
@@ -96,11 +165,20 @@ let main =
   (* let prog = 
      Let("x", Int(3),
         Let("f", Abs("y", Plus(Var("x"), Var("y"))), 
-            App(Var("f"), Var("x1")))) in  *)
+            App(Var("f"), Var("x")))) in  *)
+  (* let prog = 
+     Let("f", Abs("y", Plus(Int(2), Var("y"))), 
+        App(Var("f"), Int(3))) in  *)
   let prog = 
-    Let("f", Abs("y", Plus(Int(2), Var("y"))), 
-        App(Var("f"), Int(3))) in 
+    Let("x", Int(3),
+        Let("f", Abs("y", Plus(Var("x"), Var("y"))), 
+            Let("x", Int(5), 
+                Let("g", Abs("z", Let("x", App(Var("f"), Var("x")), App(Var("f"), Var("x")))),
+                    Let("x", Int(7), 
+                        Let("y", Int(9),
+                            Plus(App(Var("g"), Var("y")), App(Var("f"), Var("x"))))))))) in 
   let prog' = remove_lets prog in 
-  let () = prog' |> expr_to_str |> print_endline in 
-  let end_val = eval prog' [] true in 
+  let dynamic = translate_to_dynamic prog' (DynCtx([])) in 
+  let () = dynamic |> expr_to_str |> print_endline in 
+  let end_val = eval dynamic true in 
   end_val |> val_to_str |> print_endline
